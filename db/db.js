9a89b42236adf1e226b6afc7b6e6eb74dda1dbf2
AvetikNersisyan <avetik.nersisyan@gmail.com>
2023-07-06 17:25:14 +0400

    port from env

--- db/db.js
+++ db/db.js
@@ -7,9 +7,10 @@ const dbName = process.env.DB_NAME;
 const dbUsername = process.env.DB_USERNAME;
 const dbPass = process.env.DB_PASS;
 const host = process.env.DB_HOST;
+const port = process.env.DB_PORT;
 
 export const sequelize = new Sequelize(dbName, dbUsername, dbPass, {
   dialect: "postgres",
-  port: "5432",
+  port: port,
   host: host,
 });

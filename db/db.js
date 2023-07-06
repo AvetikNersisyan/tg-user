885c37870c477cc326f6f099821f6a070ef400a6
AvetikNersisyan <nersisyan.avetik@gmail.com>
2023-07-06 22:38:27 +0400

    fix users

--- db/db.js
+++ db/db.js
@@ -1,5 +1,5 @@
 import { Sequelize } from "sequelize";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -8,12 +8,6 @@ const dbUsername = process.env.DB_USERNAME;
 const dbPass = process.env.DB_PASS;
 const host = process.env.DB_HOST;
 const port = process.env.DB_PORT;
-
-console.log(port, '-------port ------- 22222222-----------');
-console.log(dbUsername, '-------dbUsername ------- 22222222-----------');
-console.log(dbPass, '-------dbPass ------- 22222222-----------');
-console.log(host, '-------host ------- 22222222-----------');
-console.log(dbName, '-------dbName ------- 22222222-----------');
 
 export const sequelize = new Sequelize(dbName, dbUsername, dbPass, {
   dialect: "postgres",
--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(mainRoute);
 app.use(errorHandling);
 
 app.get("/", (req, res, next) => {
-  res.send({ data: "Actions!" });
+  res.send({ data: "action" });
 });
 
 const synchronize = async () => {

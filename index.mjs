486aba558aa115c0920d2cd4e2e670350c162863
AvetikNersisyan <nersisyan.avetik@gmail.com>
2023-07-08 01:11:04 +0400

    pipeline

--- index.mjs
+++ index.mjs
@@ -20,7 +20,7 @@ app.use(mainRoute);
 app.use(errorHandling);
 
 app.get("/", (req, res, next) => {
-  res.send({ data: "Hoorrraaaayyy yess!!!" });
+  res.send({ success: true, data: "ok" });
 });
 
 const synchronize = async () => {

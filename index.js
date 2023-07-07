5fd3fa00ab22e4caf4778e98a5d9f6935d6e86e3
AvetikNersisyan <nersisyan.avetik@gmail.com>
2023-07-08 00:33:42 +0400

    test actions 5

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(mainRoute);
 app.use(errorHandling);
 
 app.get("/", (req, res, next) => {
-  res.send({ data: "Hiii!!!" });
+  res.send({ data: "Helllll nooooo!!!" });
 });
 
 const synchronize = async () => {
